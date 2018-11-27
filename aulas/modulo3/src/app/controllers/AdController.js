4b82dc3b47101b7c17ccb79be3aae26a1e5cdaf5
Bruno Flegler Dal Col <bruno.flegler@gmail.com>
2018-11-27 08:13:32 -0200

    add filters and pagination

--- aulas/modulo3/src/app/controllers/AdController.js
+++ aulas/modulo3/src/app/controllers/AdController.js
@@ -2,7 +2,33 @@ const Ad = require('../models/Ad')
 
 class AdController {
   async index (req, res) {
-    const ads = await Ad.find()
+    const { ...query } = req.query
+    const filters = {}
+
+    if (query.price_min || query.price_max) {
+      filters.price = {}
+
+      if (query.price_min) {
+        filters.price.$gte = query.price_min
+      }
+
+      if (query.price_max) {
+        filters.price.$lte = query.price_max
+      }
+    }
+
+    if (query.title) {
+      filters.title = new RegExp(query.title, 'i')
+    }
+
+    console.log(filters)
+
+    const ads = await Ad.paginate(filters, {
+      page: req.query.page || 1,
+      limit: 20,
+      populate: ['author'],
+      sort: '-createAt'
+    })
     res.json(ads)
   }
 
